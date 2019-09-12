531c175a47a3078405f358b0f2e9465509458f78
user <aldona.klima@gmail.com>
2019-09-12 12:09:29 +0200

    ajout des imports

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertTrue;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
