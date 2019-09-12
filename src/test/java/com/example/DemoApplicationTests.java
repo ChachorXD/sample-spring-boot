4b6ca3fc8911b7f80aaef0c72ad00e3666556f6d
user <aldona.klima@gmail.com>
2019-09-12 12:20:53 +0200

    tests

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBoottTest
 public class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
